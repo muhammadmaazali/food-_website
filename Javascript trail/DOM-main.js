5f0a99fb166579986fe43b14b980a33e34191616
Muhammad Maaz Ali <muhammadmaazali112@gmail.com>
2022-08-11 23:04:43 +0500

    DOM

--- Javascript trail/DOM-main.js
+++ Javascript trail/DOM-main.js
@@ -1,5 +1,29 @@
+// var element;
+
+// element=document.getElementsByClassName("content")[0].innerHTML;
+// console.log(element);
+
+// var element;
+
+// element=document.getElementsByClassName("content")[0].getAttribute("id");
+// console.log(element);
+
+// var element;
+
+// element=document.getElementsByClassName("content")[0].getAttribute("onclick");
+// console.log(element);
+
+// var element;
+
+// element=document.getElementsByClassName("content")[0].getAttributeNode("style").value;
+// console.log(element);
+
+// var element;
+
+// element=document.getElementsByClassName("content")[0].attributes[3];
+// console.log(element);
+
 var element;
 
-element=document.getElementsByClassName("header").innerText;
-
+element=document.getElementsByClassName("content")[0].attributes[3].name;
 console.log(element);
