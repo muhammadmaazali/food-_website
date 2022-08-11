6b0bcef79f86966ccf29b3f8271248b2eeb19682
Muhammad Maaz Ali <muhammadmaazali112@gmail.com>
2022-08-12 01:51:56 +0500

    DOM queryselector

--- Javascript trail/DOM-main.js
+++ Javascript trail/DOM-main.js
@@ -71,14 +71,41 @@
 // element=document.getElementsByClassName("header")[0].getAttribute("style");
 // console.log(element);
 
-var element;
+// var element;
 
-element=document.getElementsByClassName("header")[0].innerHTML="<h1>This Javascript</h1>";
+// element=document.getElementsByClassName("header")[0].innerHTML="<h1>This Javascript</h1>";
 
-element=document.getElementsByClassName("header")[0].removeAttribute("style");
+// element=document.getElementsByClassName("header")[0].removeAttribute("style");
 
-element=document.getElementsByClassName("header")[0].removeAttribute("class");
+// element=document.getElementsByClassName("header")[0].removeAttribute("class");
 
 
-element=document.getElementsByClassName("header")[0].getAttribute("class");
-console.log(element);
+// element=document.getElementsByClassName("header")[0].getAttribute("class");
+// console.log(element);
+
+
+// var element;
+
+// element=document.querySelector(".header").innerHTML="<h1>This Javascript</h1>";
+
+// element=document.querySelectorAll("#main li")[3].innerText;
+// console.log(element);
+
+// var element;
+
+// element=document.querySelector(".header").style.backgroundColor="black";
+// element=document.querySelector(".header").style.color="white";
+// element=document.querySelector(".header").style.padding="10px";
+
+// var element;
+
+// element=document.querySelector(".header").style.backgroundColor="black";
+// element=document.querySelector(".header").style.color="white";
+// element=document.querySelector(".header").style.padding="10px";
+// element=document.querySelector(".header").id="khang";
+// element=document.querySelector("#khang").style.color="red";
+
+
+// element=document.querySelector(".header").id;
+
+// console.log(element);
