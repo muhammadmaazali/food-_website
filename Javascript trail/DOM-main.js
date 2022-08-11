ddb3358885598b6c0ce95669fe23dab09cc7bc30
Muhammad Maaz Ali <muhammadmaazali112@gmail.com>
2022-08-12 00:37:56 +0500

    DOm set inner html

--- Javascript trail/DOM-main.js
+++ Javascript trail/DOM-main.js
@@ -23,7 +23,18 @@
 // element=document.getElementsByClassName("content")[0].attributes[3];
 // console.log(element);
 
+// var element;
+
+// element=document.getElementsByClassName("content")[0].attributes[3].name;
+// console.log(element);
+
 var element;
 
-element=document.getElementsByClassName("content")[0].attributes[3].name;
+element=document.getElementsByClassName("header")[0].innerHTML;
+console.log(element);
+
+element=document.getElementsByClassName("header")[0].innerHTML="<h1>This Javascript</h1>";
+
+
+element=document.getElementsByClassName("header")[0].innerText;
 console.log(element);
