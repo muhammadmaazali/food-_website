8e844debecf8ba37242581d79e8fa96cc820b0b7
Muhammad Maaz Ali <muhammadmaazali112@gmail.com>
2022-08-12 01:09:49 +0500

    get and aset attributees

--- Javascript trail/DOM-main.js
+++ Javascript trail/DOM-main.js
@@ -28,13 +28,57 @@
 // element=document.getElementsByClassName("content")[0].attributes[3].name;
 // console.log(element);
 
-var element;
+// var element;
 
-element=document.getElementsByClassName("header")[0].innerHTML;
-console.log(element);
+// element=document.getElementsByClassName("header")[0].innerHTML;
+// console.log(element);
+
+// element=document.getElementsByClassName("header")[0].innerHTML="<h1>This Javascript</h1>";
+
+
+// element=document.getElementsByClassName("header")[0].innerText;
+// console.log(element);
+
+// var element;
+
+// element=document.getElementsByClassName("header")[0].innerHTML="<h1>This Javascript</h1>";
+
+// element=document.getElementsByClassName("header")[0].setAttribute("id","sanam");
+
+
+// element=document.getElementsByClassName("header")[0].getAttribute("id");
+// console.log(element);
+
+// var element;
+
+// element=document.getElementsByClassName("header")[0].innerHTML="<h1>This Javascript</h1>";
+
+// element=document.getElementsByClassName("header")[0].setAttribute("style","border:20px dotted yellow");
+
+
+// element=document.getElementsByClassName("header")[0].getAttribute("style");
+// console.log(element);
+
+
+
+// var element;
+
+// element=document.getElementsByClassName("header")[0].innerHTML="<h1>This Javascript</h1>";
+
+// element=document.getElementsByClassName("header")[0].attributes[2].value="border:20px dotted yellow";
+
+
+// element=document.getElementsByClassName("header")[0].getAttribute("style");
+// console.log(element);
+
+var element;
 
 element=document.getElementsByClassName("header")[0].innerHTML="<h1>This Javascript</h1>";
 
+element=document.getElementsByClassName("header")[0].removeAttribute("style");
 
-element=document.getElementsByClassName("header")[0].innerText;
+element=document.getElementsByClassName("header")[0].removeAttribute("class");
+
+
+element=document.getElementsByClassName("header")[0].getAttribute("class");
 console.log(element);
