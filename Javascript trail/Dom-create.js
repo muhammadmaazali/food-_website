734f2df56a74ffefc5dbfb0763f2c8a67a0c7e5b
Muhammad Maaz Ali <muhammadmaazali112@gmail.com>
2022-08-24 15:14:33 +0500

    DOM create

---
+++ Javascript trail/Dom-create.js
@@ -0,0 +1,37 @@
+// var newElement=document.createElement("li");
+
+// var newText=document.createTextNode("America");
+
+// newElement.appendChild(newText);
+
+// var target=document.getElementById("list");
+
+// var oldElement=target.children[0];
+// target.removeChild(oldElement);
+
+// var target=document.getElementById("list").children[2];
+
+// var copyElement=target.cloneNode(true);
+
+// document.getElementById("list2").appendChild(copyElement);
+
+// var parenElement=document.getElementById("test");
+
+// // var target=document.getElementById("abc");
+
+// var find= parenElement.contains(abc);
+
+// console.log(find);
+
+// var target= document.getElementById("test");
+
+// var find=target.hasAttribute("class");
+
+// console.log(find);
+
+
+var target= document.getElementById("test");
+
+var find=target.hasChildNodes();
+
+console.log(find);
